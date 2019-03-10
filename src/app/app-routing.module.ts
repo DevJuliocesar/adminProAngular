055d307d7eef93bcbd21adefec63a0c013596687
Julio Cesar Maldonado Moreno <jcmaldonadom@ufpso.edu.co>
2019-03-10 03:38:53 -0500

    primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Survey1Component } from './pages/survey1/survey1.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'progress', component: ProgressComponent},
+      {path: 'grafica1', component: Survey1Component},
+      {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+    ]
+  },
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NopagefoundComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
